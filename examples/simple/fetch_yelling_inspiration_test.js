1a96286a8593357fc4c940489ff6ad53a26acddf
Dany Haddad <danyhaddad43@gmail.com>
2017-02-23 19:16:45 -0800

    added example showing bad testing

---
+++ examples/simple/fetch_yelling_inspiration_test.js
@@ -0,0 +1,23 @@
+"use strict"
+
+const quoteOfTheDay = "get up. because mickey loves ya"
+const expectedYelling = "GET UP. BECAUSE MICKEY LOVES YA"
+
+const assert = require('assert')
+const rewire = require('rewire')
+const fetchYellingInspiration = rewire('./fetch_yelling_inspiration')
+
+describe('fetchYellingInspiration', function() {
+  beforeEach(function() {
+    this.revertRewire = fetchYellingInspiration.__set__({
+      fetchInspiration: function(done) {
+        done(null, quoteOfTheDay)
+      }
+    })
+  })
+  it('yells at you, but is inspiring', function () {
+    fetchYellingInspiration(function(err, yellingQuote) {
+      assert.equal(yellingQuote, expectedYelling)
+    })
+  })
+})
--- examples/simple/plus_one.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-
-// const plus = require('./plus')
-// module.exports = function (x) {
-//   return plus(x, 1)
-// }
